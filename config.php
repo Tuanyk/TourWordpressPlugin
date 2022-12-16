19a783be5831d130625a6b664bf5d92e36b64160
Tuanyk <nguyenhuuhoangtuan.ftu@gmail.com>
2022-12-16 17:53:00 +0700

    add config category

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php 
+
+function tours_config()
+{
+    $data = array(
+        'slug' => 'tour',
+        'singular_name' => 'Tour',
+        'name' => 'Tours',
+    );
+    return $data;
+}
+
+function tours_category_config()
+{
+    $data = array(
+        'slug' => 'tour-category',
+        'singular_name' => 'Tour Category',
+        'name' => 'Tour Category',
+    );
+    return $data;
+}
